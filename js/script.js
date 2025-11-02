2d74942fe2bf92004a11b7cdc5d36ab0a6474798
Rudson Manenti Valim <rudsonmanenti3@gmail.com>
2025-11-02 11:03:39 -0200

    atualização js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
  // Atualiza o ano automaticamente no rodapé
     document.getElementById('year').textContent = new Date().getFullYear();
+
+
+  // Seleciona os elementos
+  const hamb = document.getElementById('hamb');
+  const nav = document.getElementById('nav');
+
+  // Alterna visibilidade do menu ao clicar no ícone
+  hamb.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+
+  // Fecha o menu ao clicar em um link
+  const links = document.querySelectorAll('#nav a');
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      nav.classList.remove('active');
+    });
+  });
+
