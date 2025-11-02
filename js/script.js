9d3666dfbf249363952b2ca7791de39c0ca41372
Rudson Manenti Valim <rudsonmanenti3@gmail.com>
2025-11-02 11:12:39 -0200

    atualização

--- js/script.js
+++ js/script.js
@@ -2,20 +2,18 @@
     document.getElementById('year').textContent = new Date().getFullYear();
 
 
-  // Seleciona os elementos
-  const hamb = document.getElementById('hamb');
+ const hamb = document.getElementById('hamb');
   const nav = document.getElementById('nav');
 
-  // Alterna visibilidade do menu ao clicar no ícone
   hamb.addEventListener('click', () => {
     nav.classList.toggle('active');
+    hamb.classList.toggle('open'); // adiciona classe ao botão
   });
 
-  // Fecha o menu ao clicar em um link
-  const links = document.querySelectorAll('#nav a');
-  links.forEach(link => {
+  document.querySelectorAll('#nav a').forEach(link => {
     link.addEventListener('click', () => {
       nav.classList.remove('active');
+      hamb.classList.remove('open'); // volta o ícone para ☰
     });
   });
 
